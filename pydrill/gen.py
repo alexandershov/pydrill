f70f244fd66ddabf1a8d74a00f80d9bfa26c3cde
aershov <codumentary.com@gmail.com>
2015-06-16 14:30:19 +0300

    Add nested-comprehensions question

--- pydrill/gen.py
+++ pydrill/gen.py
@@ -1,3 +1,4 @@
+from functools import partial
 import random
 
 
@@ -5,9 +6,20 @@ def integer(min_value, max_value):
     return random.randint(min_value, max_value)
 
 
-def var_name():
-    return random.choice(['x', 'y', 'z'])
+def var_name(n=1):
+    var_names = ['x', 'y', 'z']
+    if n == 1:
+        return random.choice(var_names)
+    return random.sample(var_names, n)
 
 
 def choice(seq):
     return random.choice(seq)
+
+
+def nested_list(num_lists=2, num_items=2, make_value=partial(integer, 1, 5), with_flat=True):
+    nested = [[make_value() for j in range(num_items)]
+              for i in range(num_lists)]
+    if with_flat:
+        return nested, sum(nested, [])
+    return nested
