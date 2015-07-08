916b8a8bbcaf8649626e82254f60c8553c70b2e1
aershov <codumentary.com@gmail.com>
2015-07-08 10:34:56 +0300

    Commit explicitly in manage.load_questions

--- pydrill/manage.py
+++ pydrill/manage.py
@@ -1,6 +1,6 @@
 from flask.ext.script import Manager
 
-from pydrill import app
+from pydrill import app, db
 from pydrill import models
 
 
@@ -9,12 +9,11 @@ manager = Manager(app)
 
 @manager.command
 def load_questions(directory):
-    """Delete current set of questions
-    load questions from the specified directory.
-    """
-    models.Answer.query.delete()
-    models.Question.query.delete()
+    """Replace current set of questions with questions from the specified directory."""
+    for model in [models.Answer, models.Question]:
+        model.query.delete()
     models.load_questions(directory)
+    db.session.commit()  # models.load_questions() commits itself, but better be explicit
 
 
 if __name__ == '__main__':
